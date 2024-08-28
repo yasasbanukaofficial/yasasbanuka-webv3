df5f87bc030432767bf42ad8efc6853b243ca0fc
Yasas Banuka <yasasbanukaofficial@gmail.com>
2024-08-28 22:34:34 +0530

    fixing errors #2 in config

--- next.config.js
+++ next.config.js
@@ -6,23 +6,21 @@ const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
     output: 'export',
 
-    // Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+    // Set the basePath and assetPrefix for GitHub Pages
+    //basePath: isProd ? '/yasasbanuka-webv3' : '',
+    //assetPrefix: isProd ? '/yasasbanuka-webv3/' : '',
+
+    // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
     trailingSlash: true,
 
-    // Prevent automatic `/me` -> `/me/`, instead preserve `href`
+    // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
     skipTrailingSlashRedirect: true,
 
-    // Change the output directory `out` -> `dist`
+    // Optional: Change the output directory `out` -> `dist`
     distDir: 'dist',
-
-    // Ensure images are unoptimized since GitHub Pages doesn't support Next.js Image Optimization
     images: {
-        unoptimized: true,
+        unoptimized: true, // Disable image optimization for static exports
     },
-
-    // Set the base path and asset prefix for GitHub Pages
-    basePath: isProd ? '/yasasbanuka-webv3' : '',
-    assetPrefix: isProd ? '/yasasbanuka-webv3/' : '',
-}
+};
 
 module.exports = nextConfig;
