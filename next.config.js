0d790ba0795b929ea9bb751e2bc63c015b09381c
Yasas Banuka <yasasbanukaofficial@gmail.com>
2024-08-28 22:23:52 +0530

    fixed resolution errors and updating config

--- next.config.js
+++ next.config.js
@@ -1,20 +1,28 @@
 /**
  * @type {import('next').NextConfig}
  */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
     output: 'export',
 
-    // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+    // Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
     trailingSlash: true,
 
-    // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+    // Prevent automatic `/me` -> `/me/`, instead preserve `href`
     skipTrailingSlashRedirect: true,
 
-    // Optional: Change the output directory `out` -> `dist`
+    // Change the output directory `out` -> `dist`
     distDir: 'dist',
+
+    // Ensure images are unoptimized since GitHub Pages doesn't support Next.js Image Optimization
     images: {
-        unoptimized: true
-    }
+        unoptimized: true,
+    },
+
+    // Set the base path and asset prefix for GitHub Pages
+    basePath: isProd ? '/your-repo-name' : '',
+    assetPrefix: isProd ? '/your-repo-name/' : '',
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
