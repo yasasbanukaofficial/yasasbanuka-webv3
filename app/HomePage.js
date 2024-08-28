c11d8a2ed390fa363ed6e32c8a426c8c4f7b2ae4
Yasas Banuka <yasasbanukaofficial@gmail.com>
2024-08-28 15:31:44 +0530

    added about.js & css

--- app/HomePage.js
+++ app/HomePage.js
@@ -2,13 +2,19 @@ import React from 'react'
 import styles from './HomePage.module.css';
 import Navbar from './components/common/Navbar';
 import Hero from './components/organisms/Hero';
+import About from './components/organisms/About';
 
 const HomePage = () => {
     return (
-        <div className={styles.Home}>
-            <Navbar />
-            <Hero />
-        </div>
+        <>
+            <div className={styles.Home}>
+                <Navbar />
+                <Hero />
+            </div>
+            <div className={styles.Home}>
+                <About />
+            </div>
+        </>
     )
 }
 
